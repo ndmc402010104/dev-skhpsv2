f453ead92af405936d005eff7dc547cb59a9b070
益昇 <ndmc402010104@gmail.com>
2026-06-12 00:57:09 +0800

    完成初步架構及runtime顯示

--- version.js
+++ version.js
@@ -1,11 +1,11 @@
 window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v.2.0.0.2-202606120056",
+  version: "v.2.0.0.3-202606120057",
   major: 2,
   minor: 0,
   patch: 0,
-  build: 2,
-  buildAt: "202606120056",
-  updatedAt: "2026-06-12 00:56",
+  build: 3,
+  buildAt: "202606120057",
+  updatedAt: "2026-06-12 00:57",
   source: "version.js"
 };

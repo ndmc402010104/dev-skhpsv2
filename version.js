c382401479d2d3bc573233d1f32a4accd5974725
益昇 <ndmc402010104@gmail.com>
2026-06-13 02:30:48 +0800

    chore: update skhpsv2 runtime/version v2.2.6-202606130230

--- version.js
+++ version.js
@@ -1,10 +1,10 @@
 window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.2.5-202606130211",
+  version: "v2.2.6-202606130230",
   major: 2,
   minor: 2,
-  patch: 5,
-  buildTime: "202606130211",
-  updatedAt: "2026-06-13T02:11:00+08:00",
+  patch: 6,
+  buildTime: "202606130230",
+  updatedAt: "2026-06-13T02:30:00+08:00",
   source: "version.js"
 };

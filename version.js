e2c3f3a2f2a740890cdc8dbe2349d7c839d663e3
益昇 石 <ndmc402010104@gmail.com>
2026-06-16 10:44:14 +0800

    修正cache版本號問題

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.4.4-202606161043",
+  version: "v2.4.5-202606161044",
   major: 2,
   minor: 4,
-  patch: 4,
-  buildTime: "202606161043",
-  updatedAt: "2026-06-16T10:43:39+08:00",
+  patch: 5,
+  buildTime: "202606161044",
+  updatedAt: "2026-06-16T10:44:07+08:00",
   source: "version.js"
 };
+
 
 
 

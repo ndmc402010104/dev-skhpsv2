289101ee0ecaf605286d2307410ddeeb66051ed3
益昇 <ndmc402010104@gmail.com>
2026-06-19 18:31:17 +0800

    修正loading奇怪顏色及跳動

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.2-202606191537",
+  version: "v2.7.3-202606191831",
   major: 2,
   minor: 7,
-  patch: 2,
-  buildTime: "202606191537",
-  updatedAt: "2026-06-19T15:37:06+08:00",
+  patch: 3,
+  buildTime: "202606191831",
+  updatedAt: "2026-06-19T18:31:05+08:00",
   source: "version.js"
 };
+
 
 
 

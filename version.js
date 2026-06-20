d111b2033759c17f542784bc34e2dd23a8cb1cc5
益昇 <ndmc402010104@gmail.com>
2026-06-20 10:26:30 +0800

    修正背景奇怪的白變藍

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.8-202606201026",
+  version: "v2.7.9-202606201026",
   major: 2,
   minor: 7,
-  patch: 8,
+  patch: 9,
   buildTime: "202606201026",
-  updatedAt: "2026-06-20T10:26:09+08:00",
+  updatedAt: "2026-06-20T10:26:27+08:00",
   source: "version.js"
 };
+
 
 
 

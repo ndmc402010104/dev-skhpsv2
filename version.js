b14c677e3f88a813a6490641f4809fd8161e55ab
益昇 <ndmc402010104@gmail.com>
2026-06-20 10:37:49 +0800

    修正顏色不一致

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.9-202606201026",
+  version: "v2.7.9-202606201037",
   major: 2,
   minor: 7,
   patch: 9,
-  buildTime: "202606201026",
-  updatedAt: "2026-06-20T10:26:27+08:00",
+  buildTime: "202606201037",
+  updatedAt: "2026-06-20T10:37:40+08:00",
   source: "version.js"
 };
+
 
 
 

234f7988dbffbff0366ac4138edfcdfc686e40f4
益昇 石 <ndmc402010104@gmail.com>
2026-06-22 07:56:26 +0800

    update skhpsv2 v2.7.14-202606220756

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.14-202606220751",
+  version: "v2.7.14-202606220756",
   major: 2,
   minor: 7,
   patch: 14,
-  buildTime: "202606220751",
-  updatedAt: "2026-06-22T07:51:30+08:00",
+  buildTime: "202606220756",
+  updatedAt: "2026-06-22T07:56:22+08:00",
   source: "version.js"
 };
+
 
 
 

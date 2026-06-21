4abbc69e5506dbabc7fae3883ea52c4c40f19d01
益昇 石 <ndmc402010104@gmail.com>
2026-06-22 07:51:19 +0800

    修正會議簽到異常

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.12-202606212310",
+  version: "v2.7.13-202606220751",
   major: 2,
   minor: 7,
-  patch: 12,
-  buildTime: "202606212310",
-  updatedAt: "2026-06-21T23:10:17+08:00",
+  patch: 13,
+  buildTime: "202606220751",
+  updatedAt: "2026-06-22T07:51:06+08:00",
   source: "version.js"
 };
+
 
 
 

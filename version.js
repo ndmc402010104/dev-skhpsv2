e2126018a0b88ff2e06c2012960f4f1436af29cd
益昇 石 <ndmc402010104@gmail.com>
2026-06-22 08:54:49 +0800

    修正簽到後台table顯示問題

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.15-202606220854",
+  version: "v2.7.16-202606220854",
   major: 2,
   minor: 7,
-  patch: 15,
+  patch: 16,
   buildTime: "202606220854",
-  updatedAt: "2026-06-22T08:54:23+08:00",
+  updatedAt: "2026-06-22T08:54:44+08:00",
   source: "version.js"
 };
+
 
 
 

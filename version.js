46eaa1de13dbc9f77ee11af118a70b7bc2944618
益昇 石 <ndmc402010104@gmail.com>
2026-06-24 13:29:34 +0800

    0624暫時完成版

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.26-202606241117",
+  version: "v2.7.27-202606241329",
   major: 2,
   minor: 7,
-  patch: 26,
-  buildTime: "202606241117",
-  updatedAt: "2026-06-24T11:17:37+08:00",
+  patch: 27,
+  buildTime: "202606241329",
+  updatedAt: "2026-06-24T13:29:21+08:00",
   source: "version.js"
 };
+
 
 
 

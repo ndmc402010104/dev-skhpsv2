3e05ac60cafee7c7b32932c437968d7997022337
益昇 <ndmc402010104@gmail.com>
2026-06-28 00:02:46 +0800

    暫時穩定版可以push正式版

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.7.29-202606251459",
+  version: "v2.7.30-202606280002",
   major: 2,
   minor: 7,
-  patch: 29,
-  buildTime: "202606251459",
-  updatedAt: "2026-06-25T14:59:34+08:00",
+  patch: 30,
+  buildTime: "202606280002",
+  updatedAt: "2026-06-28T00:02:22+08:00",
   source: "version.js"
 };
+
 
 
 

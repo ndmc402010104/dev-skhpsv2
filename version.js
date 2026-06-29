e8d61dd9ddf42dcc22ba9c582d953e0a1ee90ece
益昇 <ndmc402010104@gmail.com>
2026-06-29 23:42:55 +0800

    測試delay 700ms

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.8.0-202606292329",
+  version: "v2.8.1-202606292342",
   major: 2,
   minor: 8,
-  patch: 0,
-  buildTime: "202606292329",
-  updatedAt: "2026-06-29T23:29:32+08:00",
+  patch: 1,
+  buildTime: "202606292342",
+  updatedAt: "2026-06-29T23:42:44+08:00",
   source: "version.js"
 };
+
 
 
 

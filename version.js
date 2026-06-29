425e912699f5aeb4daa3c1f20d72ce642fabb219
益昇 <ndmc402010104@gmail.com>
2026-06-29 23:56:58 +0800

    修正由快速登入登入系統出現的異常

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.8.2-202606292345",
+  version: "v2.8.3-202606292356",
   major: 2,
   minor: 8,
-  patch: 2,
-  buildTime: "202606292345",
-  updatedAt: "2026-06-29T23:45:48+08:00",
+  patch: 3,
+  buildTime: "202606292356",
+  updatedAt: "2026-06-29T23:56:36+08:00",
   source: "version.js"
 };
+
 
 
 

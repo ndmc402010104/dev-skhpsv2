b5a57ecac7efa166ea14f162fc6df453c51d0cc6
益昇 <ndmc402010104@gmail.com>
2026-06-30 10:25:10 +0800

    修正跳轉員工專區首頁流程

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.8.3-202606292356",
+  version: "v2.8.4-202606301024",
   major: 2,
   minor: 8,
-  patch: 3,
-  buildTime: "202606292356",
-  updatedAt: "2026-06-29T23:56:36+08:00",
+  patch: 4,
+  buildTime: "202606301024",
+  updatedAt: "2026-06-30T10:24:57+08:00",
   source: "version.js"
 };
+
 
 
 

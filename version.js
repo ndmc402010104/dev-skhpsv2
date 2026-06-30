275acf6002330c081cbfbfaa79c6ee1f1f7382ba
益昇 <ndmc402010104@gmail.com>
2026-06-30 16:08:03 +0800

    更新helper版本

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.8.6-202606301559",
+  version: "v2.8.7-202606301607",
   major: 2,
   minor: 8,
-  patch: 6,
-  buildTime: "202606301559",
-  updatedAt: "2026-06-30T15:59:35+08:00",
+  patch: 7,
+  buildTime: "202606301607",
+  updatedAt: "2026-06-30T16:07:54+08:00",
   source: "version.js"
 };
+
 
 
 

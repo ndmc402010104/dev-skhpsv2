743e1ca2e3b69dea384b8be081d11f87e7478005
益昇 <ndmc402010104@gmail.com>
2026-07-02 00:12:37 +0800

    不知道為甚麼剛剛沒有上船

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.9.0-202607020011",
+  version: "v2.9.0-202607020012",
   major: 2,
   minor: 9,
   patch: 0,
-  buildTime: "202607020011",
-  updatedAt: "2026-07-02T00:11:53+08:00",
+  buildTime: "202607020012",
+  updatedAt: "2026-07-02T00:12:30+08:00",
   source: "version.js"
 };
+
 
 
 

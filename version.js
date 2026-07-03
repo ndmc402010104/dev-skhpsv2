89c6a8a5d120f57f47f20902faf3725fe09f2dab
益昇 <ndmc402010104@gmail.com>
2026-07-03 22:58:57 +0800

    還沒有上傳成功

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.10.0-202607032255",
+  version: "v2.10.0-202607032258",
   major: 2,
   minor: 10,
   patch: 0,
-  buildTime: "202607032255",
-  updatedAt: "2026-07-03T22:55:31+08:00",
+  buildTime: "202607032258",
+  updatedAt: "2026-07-03T22:58:47+08:00",
   source: "version.js"
 };
+
 
 
 

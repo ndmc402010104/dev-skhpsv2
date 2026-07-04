b55c387dd9b1db6226297c9e23de811a58c73edf
益昇 <ndmc402010104@gmail.com>
2026-07-05 02:23:49 +0800

    修改footer計算

--- version.js
+++ version.js
@@ -1,13 +1,14 @@
 ﻿window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.11.2-202607050223",
+  version: "v2.11.3-202607050223",
   major: 2,
   minor: 11,
-  patch: 2,
+  patch: 3,
   buildTime: "202607050223",
-  updatedAt: "2026-07-05T02:23:29+08:00",
+  updatedAt: "2026-07-05T02:23:46+08:00",
   source: "version.js"
 };
+
 
 
 

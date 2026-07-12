bd7324929ce416a92082861d854a064282ae0549
益昇 <ndmc402010104@gmail.com>
2026-07-12 16:43:16 +0800

    chore(dev): bump version for CSS Setting route

--- version.js
+++ version.js
@@ -1,16 +1,16 @@
 ﻿/*
 檔案位置：skhpsv2/version.js
-時間戳：2026-07-12 12:24 UTC+8
+時間戳：2026-07-12 16:43 UTC+8
 用途：SKHPSv2 dev 版本；加入 Jonaminz bridge 與 CSS package runtime 相容層。
 */
 window.SKHPS_VERSION = {
   appId: "skhpsv2",
-  version: "v2.12.0-202607121224",
+  version: "v2.12.1-202607121643",
   major: 2,
   minor: 12,
-  patch: 0,
-  buildTime: "202607121224",
-  updatedAt: "2026-07-12T12:24:00+08:00",
+  patch: 1,
+  buildTime: "202607121643",
+  updatedAt: "2026-07-12T16:43:00+08:00",
   source: "version.js"
 };
 
